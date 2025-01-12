8e43f4e21105d63c95e928514f2300f7d642aeb7
김도형 <shapekim98@gmail.com>
2025-01-12 17:35:55 +0900

    [feat] 가입코드 View, ViewModel 작성

---
+++ yappu-world-ios/Source/Presentation/SignUp/Code/View/CodeView.swift
@@ -0,0 +1,138 @@
+//
+//  CodeView.swift
+//  yappu-world-ios
+//
+//  Created by 김도형 on 1/12/25.
+//
+
+import SwiftUI
+
+struct CodeView: View {
+    @Bindable
+    private var viewModel: CodeViewModel
+    
+    @FocusState
+    private var focusedField: TextFieldType?
+    
+    init(viewModel: CodeViewModel) {
+        self.viewModel = viewModel
+    }
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            InformationLabel(
+                customStep: 5,
+                title: "가입코드만 입력하면 끝",
+                sub: "YAPP 신청시 전달받은 가입코드를 입력해주세요."
+            )
+            
+            codeTextFields
+                .padding(.top, 40)
+            
+            helpButton
+                .padding(.top, 40)
+            
+            Spacer()
+            
+            Button(action: {}) {
+                Text("다음")
+                    .frame(maxWidth: .infinity)
+            }
+            .disabled(viewModel.model.isValid.not())
+            .buttonStyle(.yapp(radius: 12, style: .primary))
+            .animation(.easeInOut, value: viewModel.model.isValid.not())
+        }
+        .padding(.horizontal, 20)
+        .padding(.vertical, 16)
+    }
+}
+
+private extension CodeView {
+    var codeTextFields: some View {
+        HStack(spacing: 16) {
+            Spacer()
+            
+            codeTextField(
+                $viewModel.model.code1,
+                type: .code1,
+                next: .code2
+            )
+            
+            codeTextField(
+                $viewModel.model.code2,
+                type: .code2,
+                next: .code3
+            )
+            
+            codeTextField(
+                $viewModel.model.code3,
+                type: .code3,
+                next: .code4
+            )
+            
+            codeTextField(
+                $viewModel.model.code4,
+                type: .code4
+            )
+            
+            Spacer()
+        }
+    }
+    
+    @ViewBuilder
+    func codeTextField(
+        _ code: Binding<String>,
+        type: TextFieldType,
+        next: TextFieldType? = nil
+    ) -> some View {
+        TextField("", text: code)
+            .font(.pretendard24(.bold))
+            .foregroundStyle(.labelGray)
+            .tint(.yapp_primary)
+            .multilineTextAlignment(.center)
+            .keyboardType(.numberPad)
+            .padding(8)
+            .frame(width: 48, height: 48)
+            .background(.gray22)
+            .clipRectangle(10)
+            .focused($focusedField, equals: type)
+            .onChange(of: code.wrappedValue) { oldValue, newValue in
+                guard
+                    oldValue.isEmpty && newValue.isEmpty.not(),
+                    next != nil
+                else {
+                    return
+                }
+                focusedField = next
+            }
+    }
+    
+    var helpButton: some View {
+        Button(action: {}) {
+            HStack(spacing: 4) {
+                Image(.circleQuestion)
+                    .resizable()
+                    .frame(width: 16, height: 16)
+                
+                Text("가입코드를 모르겠어요.")
+                    .font(.pretendard14(.bold))
+            }
+            .foregroundStyle(.gray60)
+        }
+    }
+}
+
+private extension CodeView {
+    enum TextFieldType: Hashable {
+        case code1
+        case code2
+        case code3
+        case code4
+    }
+}
+
+#Preview {
+    NavigationStack {
+        CodeView(viewModel: .init(model: .init()))
+    }
+}
---
+++ yappu-world-ios/Source/Presentation/SignUp/Code/ViewModel/CodeViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  CodeViewModel.swift
+//  yappu-world-ios
+//
+//  Created by 김도형 on 1/12/25.
+//
+
+import SwiftUI
+
+@Observable
+class CodeViewModel {
+    struct Model {
+        var code1: String = ""
+        var code2: String = ""
+        var code3: String = ""
+        var code4: String = ""
+        var isValid: Bool {
+            return code1.isEmpty.not()
+            && code2.isEmpty.not()
+            && code3.isEmpty.not()
+            && code4.isEmpty.not()
+        }
+        
+        init() { }
+    }
+    
+    var model: Model
+    
+    init(model: Model) {
+        self.model = model
+    }
+}
