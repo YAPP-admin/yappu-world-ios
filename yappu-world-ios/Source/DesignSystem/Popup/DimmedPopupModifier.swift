9d9d809131bcb4b8a319e380dcf540583092b8ce
김도형 <shapekim98@gmail.com>
2025-02-24 21:51:53 +0900

    [fix] 팝업 애니메이션 개선

--- yappu-world-ios/Source/DesignSystem/Popup/DimmedPopupModifier.swift
+++ yappu-world-ios/Source/DesignSystem/Popup/DimmedPopupModifier.swift
@@ -15,46 +15,44 @@ struct DimmedPopupModifier<Popup: View>: ViewModifier {
     var alignment: Alignment = .bottom
     
     func body(content: Content) -> some View {
-        ZStack {
-            content
-            
-            Group {
-                if isOpen {
-                    Color.black.opacity(0.3)
-                        .ignoresSafeArea()
-                        .onTapGesture {
-                            isOpen = false // 팝업 닫기
-                        }
-                }
-                
-                VStack {
-                    switch alignment {
-                    case .bottom:
-                        Spacer()
-                    default:
-                        EmptyView()
+        content
+            .overlay {
+                Group {
+                    if isOpen {
+                        Color.black.opacity(0.3)
+                            .ignoresSafeArea()
+                            .onTapGesture {
+                                isOpen = false // 팝업 닫기
+                            }
                     }
                     
-                    ZStack(alignment: .leading) {
-                        Color.white
-                            .frame(maxWidth: .infinity)
-                            .cornerRadius(radius: 16, corners: .allCorners)
+                    VStack {
+                        switch alignment {
+                        case .bottom:
+                            Spacer()
+                        default:
+                            EmptyView()
+                        }
                         
-                        popupView
-                            .transition(.move(edge: .bottom)) // 팝업 애니메이션 추가
-                            .padding(.horizontal, horizontalPadding)
-                            .padding(.vertical, verticalPadding)
+                        ZStack(alignment: .leading) {
+                            Color.white
+                                .frame(maxWidth: .infinity)
+                                .cornerRadius(radius: 16, corners: .allCorners)
+                            
+                            popupView
+                                .transition(.move(edge: .bottom)) // 팝업 애니메이션 추가
+                                .padding(.horizontal, horizontalPadding)
+                                .padding(.vertical, verticalPadding)
+                        }
+                        .fixedSize(horizontal: false, vertical: true)
+                        .padding(.horizontal, 8)
+                        .offset(x: 0, y: isOpen ? 0 : 15)
+                        .opacity(isOpen ? 1 : 0)
                     }
-                    .fixedSize(horizontal: false, vertical: true)
-                    .padding(.horizontal, 8)
-                    .offset(x: 0, y: isOpen ? 0 : 15)
-                    .opacity(isOpen ? 1 : 0)
+                    
                 }
-                
+                .animation(.smooth(duration: 0.2), value: isOpen) // 모든 애니메이션을 동기화
             }
-            .zIndex(999) // 팝업을 최상단으로 설정
-            .animation(.smooth(duration: 0.2), value: isOpen) // 모든 애니메이션을 동기화
-        }
     }
 }
 
@@ -96,11 +94,16 @@ extension View {
 
 #Preview {
     VStack {
+        Spacer()
+        
+        HStack { Spacer() }
         Text("Hello")
             .font(.title)
             .padding()
+        
+        Spacer()
     }
-    .yappBottomPopup(isOpen: .constant(true)) {
+    .yappDefaultPopup(isOpen: .constant(true)) {
         VStack(alignment: .leading) {
             Text("서비스 이용약관")
                 .font(.pretendard18(.bold))
