a4e87e65157aae130e228417d796875fd0588902
김도형 <shapekim98@gmail.com>
2025-03-16 19:40:39 +0900

    [feat] 알람 API 구성

---
+++ yappu-world-ios/Source/Data/Auth/DTO/DeviceAlarmRequest.swift
@@ -0,0 +1,12 @@
+//
+//  DeviceAlarmRequest.swift
+//  yappu-world-ios
+//
+//  Created by 김도형 on 3/16/25.
+//
+
+import Foundation
+
+struct DeviceAlarmRequest: Encodable {
+    let deviceToggle: Bool
+}
