0f9dc22d8d271bf21b4a2caf90479338a96b329e
김도형 <shapekim98@gmail.com>
2025-04-02 23:37:10 +0900

    ci 스크립트 추가

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#  ci_post_clone.sh
+#  Interface
+#
+#  Created by 김도형 on 2023/09/06.
+#
+
+defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
+
+cd "$SRCROOT/yappu-world-ios" || exit 1
+# base64 디코딩 후 파일 생성
+echo "$GOOGLESERVICE_BASE64" | base64 --decode > GoogleService-Info.plist
+# 파일 생성 확인
+if [ -f "GoogleService-Info.plist" ]; then
+    echo "GoogleService-Info.plist successfully created."
+else
+    echo "Failed to create GoogleService-Info.plist" >&2
+    exit 1
+fi
