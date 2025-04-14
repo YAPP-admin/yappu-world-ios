469d08eb03e1721f51840a476b8fcd9864ea5dcb
김건형 <show11837@gmail.com>
2025-04-15 00:48:21 +0900

    [Feat] 마이페이지 메인 화면 UI 구성 위한 MyPageView.swift 생성

---
+++ yappu-world-ios/Source/Presentation/MyPage/View/MyPageView.swift
@@ -0,0 +1,18 @@
+//
+//  MyPageView.swift
+//  yappu-world-ios
+//
+//  Created by 김건형 on 4/15/25.
+//
+
+import SwiftUI
+
+struct MyPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyPageView()
+}
