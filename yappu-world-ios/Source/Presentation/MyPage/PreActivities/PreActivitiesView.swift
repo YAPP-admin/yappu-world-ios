4e5a57a1c05745a35eda08d5c111403f5f27e823
김건형 <show11837@gmail.com>
2025-04-15 13:49:24 +0900

    [Feat] 마이페이지 - 이전 활동내역 화면 UI 구성 위한 PreActivitiesView.swift 생성

---
+++ yappu-world-ios/Source/Presentation/MyPage/PreActivities/PreActivitiesView.swift
@@ -0,0 +1,18 @@
+//
+//  PreActivitiesView.swift
+//  yappu-world-ios
+//
+//  Created by 김건형 on 4/15/25.
+//
+
+import SwiftUI
+
+struct PreActivitiesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PreActivitiesView()
+}
