69d1a3227034209dedc1131f960107f8feaab126
김건형 <show11837@gmail.com>
2025-04-16 01:12:12 +0900

    [Chore] 마이페이지 Menu 영역 view 파일 생성

---
+++ yappu-world-ios/Source/Presentation/MyPage/Profile/View/MyPageMenuView.swift
@@ -0,0 +1,18 @@
+//
+//  MyPageMenuView.swift
+//  yappu-world-ios
+//
+//  Created by 김건형 on 4/16/25.
+//
+
+import SwiftUI
+
+struct MyPageMenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyPageMenuView()
+}
