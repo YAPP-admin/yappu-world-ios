79b68db5bbcdfff1195cd2f57aac57dbae51eba9
김건형 <show11837@gmail.com>
2025-04-15 14:31:17 +0900

    [Feat] 마이페이지 - 이전 활동 내역 리스트 셀 UI 구성용 PreActivitiesCell.swift 생성

---
+++ yappu-world-ios/Source/Presentation/MyPage/PreActivities/View/PreActivitiesCell.swift
@@ -0,0 +1,18 @@
+//
+//  PreActivitiesCell.swift
+//  yappu-world-ios
+//
+//  Created by 김건형 on 4/15/25.
+//
+
+import SwiftUI
+
+struct PreActivitiesCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PreActivitiesCell()
+}
