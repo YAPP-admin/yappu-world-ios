6e0ba827ca00dd043b307fffaa37d43b13074b65
김건형 <show11837@gmail.com>
2025-04-15 23:42:58 +0900

    [Feat] MyPageNavigationView 구현 및 네비게이션 경로에 따른 화면 전환 처리

--- yappu-world-ios/Source/Presentation/MyPage/Profile/Router/MyPageNavigationView.swift
+++ yappu-world-ios/Source/Presentation/MyPage/Profile/Router/MyPageNavigationView.swift
@@ -5,4 +5,44 @@
 //  Created by 김건형 on 4/15/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct MyPageNavigationView: View {
+    @Bindable
+    private var router: MyPageNavigationRouter
+    
+    init(router: MyPageNavigationRouter) {
+        self.router = router
+    }
+    
+    var body: some View {
+        NavigationStack(path: $router.path) {
+            MyPageView(viewModel: router.myPageViewModel)
+                .navigationDestination(for: MyPagePath.self) { path in
+                    switch path {
+                    case .setting:
+                        if let viewModel = router.settingViewModel {
+                            SettingView(viewModel: viewModel)
+                        }
+                    case .attendances:
+                        EmptyView() // 임시
+                    case .preActivities:
+                        if let viewModel = router.preActivitesViewModel {
+                            PreActivitiesView(viewModel: viewModel)
+                        }
+                    case let .safari(url):
+                        YPSafariView<HomePath>(url: url)
+                            .ignoresSafeArea()
+                            .navigationBarBackButtonHidden()
+                    }
+                }
+        }
+        .task {
+            await router.onTask()
+        }
+    }
+}
+
+#Preview {
+    HomeNavigationView(router: .init())
+}
