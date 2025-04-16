08bb9d97d4b042d9720afca5dae67e58e9152b22
김건형 <show11837@gmail.com>
2025-04-16 15:51:07 +0900

    [Chore] 홈 출석하기 영역 view 파일 생성

---
+++ yappu-world-ios/Source/Presentation/Home/View/HomeAttendView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeAttendView.swift
+//  yappu-world-ios
+//
+//  Created by 김건형 on 4/16/25.
+//
+
+import SwiftUI
+
+struct HomeAttendView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeAttendView()
+}
