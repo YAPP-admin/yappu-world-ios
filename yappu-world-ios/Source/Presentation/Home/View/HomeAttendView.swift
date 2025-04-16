e8e51d3a82a5e5603f9bdb173761a1430af88480
김건형 <show11837@gmail.com>
2025-04-16 16:13:18 +0900

    [Feat] 공지 배너 UI 추가

--- yappu-world-ios/Source/Presentation/Home/View/HomeAttendView.swift
+++ yappu-world-ios/Source/Presentation/Home/View/HomeAttendView.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 
 struct HomeAttendView: View {
+    
+    @State
+    var viewModel: HomeViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            NoticeBanner(text: "세션 당일이예요! 활기찬 하루 되세요:)")
+        } // VStack
+        .padding(.horizontal, 20)
+        .padding(.top, 24)
+    }
+}
+// MARK: - Private UI Builders
+private extension HomeAttendView {
+    
+    func NoticeBanner(text: String) -> some View {
+        HStack(spacing: 4) {
+            Image("loudSpeaker")
+            Text(text)
+                .font(.pretendard12(.medium))
+                .foregroundStyle(.labelGray)
+        }
+        .padding(.vertical, 6)
+        .padding(.horizontal, 12)
+        .background(Color.activetyCellBackgroundColor)
+        .cornerRadius(10)
     }
 }
 
 #Preview {
-    HomeAttendView()
+    HomeAttendView(viewModel: HomeViewModel())
 }
