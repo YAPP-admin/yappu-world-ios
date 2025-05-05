5005871ff267973789c7ea81fad0b83c0f51b4a5
김건형 <show11837@gmail.com>
2025-05-06 03:06:24 +0900

    [Feat] 날짜와 출석 상태에 따른 Enum

--- yappu-world-ios/Source/Presentation/Home/Common/UpcomingSessionAttendanceState.swift
+++ yappu-world-ios/Source/Presentation/Home/Common/UpcomingSessionAttendanceState.swift
@@ -7,44 +7,42 @@
 
 import Foundation
 
-enum UpcomingSessionAttendanceState {
-    case Inactive(Date)     // 출석 불가능 + 미출석
-    case Available          // 출석 가능 + 미출석
-    case Attended           // 이미 출석함
-    case NoSession          // 세션 정보 없음
+enum UpcomingSessionAttendanceState: Equatable {
+    case Inactive_Dday         // 출석 불가능 + 미출석 + 당일
+    case Inactive_Yet(String)  // 출석 불가능 + 미출석 + 당일 이후
+    case Available             // 출석 가능 + 미출석
+    case Attended              // 이미 출석함
+    case NoSession             // 세션 정보 없음
     
     var banner: String {
         switch self {
-        case .Inactive:
-            "다가오는 세션을 준비해볼까요?"
-        case .Available, .Attended:
+        case .Inactive_Dday, .Available, .Attended:
             "세션 당일이에요! 활기찬 하루 되세요 :) "
+        case .Inactive_Yet:
+            "다가오는 세션을 준비해볼까요?"
         case .NoSession:
-            ""
-        }
-    }
-    
-    var title: String {
-        switch self {
-        case .Inactive:
-            "오늘은 \(Date().formatted(as: "MM월 dd일"))이에요."
-        case .Available, .Attended:
-            ""
-        case .NoSession:
-            ""
+            "다가오는 세션이 없어요."
         }
     }
     
     var button: String {
         switch self {
-        case .Inactive(let date):
-            "\(date.formatted(as: "MM월 dd일")) 세션예정"
+        case .Inactive_Dday:
+            "20분 전부터 출석이 가능해요"
+        case .Inactive_Yet(let date):
+            "\(date) 세션예정"
         case .Available:
             "출석하기"
         case .Attended:
             "출석완료!"
-        case .NoSession:
-            ""
+        default: ""
+        }
+    }
+    
+    var isDisabled: Bool {
+        switch self {
+        case .Inactive_Dday, .Inactive_Yet: true
+        default: false
         }
     }
 }
