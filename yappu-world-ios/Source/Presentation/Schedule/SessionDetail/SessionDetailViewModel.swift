c7764254de6258eb437325d15e30e5c2a104d56e
김건형 <show11837@gmail.com>
2025-09-26 15:49:10 +0900

    [Feat] SessionDetailViewModel.swift ViewModel 파일 생성

---
+++ yappu-world-ios/Source/Presentation/Schedule/SessionDetail/SessionDetailViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  SessionDetailViewModel.swift
+//  yappu-world-ios
+//
+//  Created by 김건형 on 9/26/25.
+//
+
+import Foundation
+import SwiftUI
+import Dependencies
+import DependenciesMacros
+
+@Observable
+class SessionDetailViewModel {
+    
+}
+// MARK: - Private Async Methods
+private extension SessionDetailViewModel {
+}
