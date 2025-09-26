9f72392b8f8002274fb7be041f175b3239b3233f
김건형 <show11837@gmail.com>
2025-09-26 15:42:11 +0900

    [Style] 파일 이름 변경

--- yappu-world-ios/Source/Presentation/Schedule/SessionDetail/SessionDetailView.swift
+++ yappu-world-ios/Source/Presentation/Schedule/SessionDetail/SessionDetailView.swift
@@ -1,5 +1,5 @@
 //
-//  SessionDetailsView.swift
+//  SessionDetailView.swift
 //  yappu-world-ios
 //
 //  Created by 김건형 on 9/26/25.
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct SessionDetailsView: View {
+struct SessionDetailView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    SessionDetailsView()
+    SessionDetailView()
 }
