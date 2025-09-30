56b59d0d8486f2dcb1803ba0ac45c235181ed37c
김건형 <show11837@gmail.com>
2025-09-30 16:48:31 +0900

    [Feat] 세션 상세 공지사항 Cell View 파일 추가

---
+++ yappu-world-ios/Source/Presentation/Schedule/SessionDetail/SessionDetailNoticeCell.swift
@@ -0,0 +1,23 @@
+//
+//  SessionDetailNoticeCell.swift
+//  yappu-world-ios
+//
+//  Created by 김건형 on 9/30/25.
+//
+
+import SwiftUI
+
+struct SessionDetailNoticeCell: View {
+    // MARK: Property
+
+    var body: some View {
+        VStack {
+        } // VStack
+        .background(Color.activityCellBackgroundColor)
+        .cornerRadius(12)
+    }
+}
+
+#Preview {
+    SessionDetailNoticeCell()
+}
